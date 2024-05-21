b9eacef89aa8c48888987aaa1f29e57bc932b9db
Sumathi S <sumathi.s@ind.xebiadigital.com>
2024-05-21 13:45:12 +0530

    react-roter

---
+++ src/App.js
@@ -0,0 +1,28 @@
+// Inside App.js
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Product from './components/Product';
+import Blog from './components/Blog';
+import NotFound from './components/NotFound';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />}>
+          <Route path="product" element={<Product />} />
+        </Route>
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/blog/:postId" element={<Blog />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
